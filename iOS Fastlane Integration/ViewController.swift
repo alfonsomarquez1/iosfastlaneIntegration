75df589e7772d9599293364dafa007e5c3b2fb88
joseangelc <jose@mariachi.io>
2020-05-20 09:16:07 -0400

    Initial Commit

---
+++ iOS Fastlane Integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Fastlane Integration
+//
+//  Created by Luis Alfonso Marquez Lecona on 5/20/20.
+//  Copyright © 2020 Softclick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
